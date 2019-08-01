1e7d335eb38fb568e4c218e5359564fac810c08e
srikanthpathlavath <srikanth.pathlavath@hexadots.in>
2019-08-01 12:57:18 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const bodyParser = require('body-parser');
 const connectDB = require("./config/db");
+const path = require('path');
 
 const auth = require("./routes/api/auth");
 const users = require("./routes/api/users");
@@ -17,13 +18,23 @@ app.use(express.json({ extended: false }));
 // Connect Database
 connectDB();
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Use Routes
 app.use("/api/auth", auth);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
